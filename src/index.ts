7abf594726ca368ea3c12197fce842d0d02c7296
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2023-02-11 19:40:40 +0100

    run local express server on port 3000 by default

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import { createApp } from './app';
 
 console.info('Server is starting...');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const server = createApp().listen(port, () =>
   console.info(`Server started${process.env.NODE_ENV === 'local' ? ` on port ${port}` : ''}.`)
 );
 
-const shutdownServer = () => {
+export const shutdownServer = () => {
   console.info('Server is stopping...');
   server.close(() => console.info('Server stopped.'));
 };

1b0f407926bd6670632b88feb354d44ceea26e2a
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2023-02-11 20:16:38 +0100

    add a basic app test

---
+++ src/app.test.ts
@@ -0,0 +1,42 @@
+import { testRequest } from './tests/jest-utils';
+
+describe('App Middlewares', () => {
+  describe('Authorizer', () => {
+    it('should return 200 for an existing public route', async () => {
+      const response = await testRequest().get('/liveness');
+      expect(response.status).toBe(200);
+    });
+
+    it('should return 200 for a protected route if an API key was provided', async () => {
+      const apiKey = process.env.API_KEY_TEST;
+      expect(apiKey).toBeDefined();
+
+      if (apiKey) {
+        const response = await testRequest().get('/').set('x-api-key', apiKey);
+        expect(response.status).toBe(200);
+      }
+    });
+
+    it('should return 401 for a protected route if no API key was provided', async () => {
+      const response = await testRequest().get('/');
+      expect(response.status).toBe(401);
+    });
+  });
+
+  describe('Error Handler', () => {
+    it('should return 401 for a non-existing route', async () => {
+      const response = await testRequest().get('/me-no-exist');
+      expect(response.status).toBe(401);
+    });
+
+    it('should return 500 if an error was thrown but not caught', async () => {
+      const apiKey = process.env.API_KEY_TEST;
+      expect(apiKey).toBeDefined();
+
+      if (apiKey) {
+        const response = await testRequest().get('/fail').set('x-api-key', apiKey); // explicit route that throws
+        expect(response.status).toBe(500);
+      }
+    });
+  });
+});

e226eb9cd66b4247cbb46c1dcf9b714be8bf215b
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2023-02-12 18:07:40 +0100

    connect app to database via sequelize

---
+++ src/services/database.ts
@@ -0,0 +1,20 @@
+import { Sequelize } from 'sequelize-typescript';
+import { Message } from '../models/Message';
+
+export const sequelizeInstance = new Sequelize({
+  username: process.env.POSTGRES_USER || 'post',
+  password: process.env.POSTGRES_PASSWORD || '',
+  database: process.env.POSTGRES_DB || 'devops',
+  port: 5432,
+  dialect: 'postgres',
+  retry: {
+    match: [/SequelizeConnectionError/],
+    max: 3,
+  },
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+  logging: process.env.PGLOGGING === 'true' ? true : false,
+  models: [Message],
+});

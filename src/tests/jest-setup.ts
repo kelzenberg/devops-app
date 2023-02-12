7db1bdad3a78f6edf8c509b6d7ed6cb755272720
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2023-02-12 18:10:04 +0100

    add missing env variables to jest setup

--- src/tests/jest-setup.ts
+++ src/tests/jest-setup.ts
@@ -1,6 +1,11 @@
 const jestSetup = (): void => {
   process.env.NODE_ENV = 'test';
   process.env.API_KEY_TEST = 'api-key-test';
+  process.env.POSTGRES_USER = process.env.POSTGRES_USER || 'post';
+  process.env.POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || 'gresql';
+  process.env.POSTGRES_DB = process.env.POSTGRES_DB || 'devops';
+  process.env.DATABASE_URL = process.env.DATABASE_URL || 'postgres://post:gresql@127.0.0.1:5432/devops';
+  process.env.PGLOGGING = 'false';
 };
 
 export default jestSetup;

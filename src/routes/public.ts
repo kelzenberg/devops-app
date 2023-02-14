04cfc383922db8c5e2a974d6c8d88dc85acc6a90
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2023-02-14 14:54:56 +0100

    add a public index.html to display messages from the db

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,6 +1,10 @@
+import fs from 'node:fs/promises';
+import path from 'node:path';
 import asyncHandler from 'express-async-handler';
 import { Request, Response, Router } from 'express';
 import { sequelizeInstance } from '../services/database';
+import { NextFunction } from 'connect';
+import { Message } from '../models/Message';
 
 const livenessHandler = async (_: Request, response: Response) => {
   await Promise.resolve();
@@ -16,6 +20,33 @@ const readinessHandler = async (_: Request, response: Response) => {
   response.status(200).send("I'm ready!");
 };
 
+const mainPageHandler = async (_: Request, response: Response, next: NextFunction) => {
+  const messages = await Message.findAll();
+  let messagesHTML = 'No messages so far!';
+
+  if (messages.length > 0) {
+    messagesHTML = messages
+      .map(
+        ({ createdAt, content, author }) =>
+          `    <li>(${createdAt.toLocaleTimeString('de-DE')}) <b>${author}</b>: <i>${content}</i></li>`
+      )
+      .join('\n');
+  }
+
+  try {
+    // eslint-disable-next-line unicorn/prefer-module
+    const data = await fs.readFile(path.normalize(__dirname + '/..//public/index.html'), { encoding: 'utf8' });
+    const modified = data.replace(/^.*%message-list%.*$/gm, messagesHTML);
+
+    response.setHeader('content-type', 'text/html');
+    response.send(modified);
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+};
+
 export const publicRoutes = Router()
+  .get('/', asyncHandler(mainPageHandler))
   .get('/liveness', asyncHandler(livenessHandler))
   .get('/readiness', asyncHandler(readinessHandler));

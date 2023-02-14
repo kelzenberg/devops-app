d9eef5d1c537ac5e9953cd64acc96ed870a7fe84
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2023-02-14 14:48:42 +0100

    fix first migration timestamp names

--- sql/V1__create_Message_table.sql
+++ sql/V1__create_Message_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE message (
     id INT PRIMARY KEY,
     author VARCHAR(255) NOT NULL,
     content VARCHAR(255) NOT NULL,
-    created timestamp NOT NULL,
-    updated timestamp DEFAULT NOW()
+    created_at timestamp NOT NULL DEFAULT NOW(),
+    updated_at timestamp DEFAULT NOW()
 );

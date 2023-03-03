f33647e9c031989c691e0caaa5eb62516f2f3e4a
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2023-03-03 18:28:59 +0100

    exclude /liveness and /readiness probes from request logger

--- src/middlewares/request-logger.ts
+++ src/middlewares/request-logger.ts
@@ -1,7 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const requestLogger = (request: Request, response: Response, next: NextFunction) => {
-  const now = new Date();
-  console.log(`[ INCOMING REQUEST ] - ${now.toISOString()} => ${request.method.toUpperCase()} '${request.url}'`);
-  next();
+  const requestUrl = request.url;
+  if (['/liveness', '/readiness'].includes(requestUrl)) {
+    next();
+  } else {
+    const now = new Date();
+    console.log(`[ INCOMING REQUEST ] - ${now.toISOString()} => ${request.method.toUpperCase()} '${requestUrl}'`);
+    next();
+  }
 };
